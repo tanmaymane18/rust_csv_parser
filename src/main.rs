b7b5158d80f6efe57d3d307c203452b5bfa35def
Tanmay Mane <tanmaymane8@gmail.com>
2025-08-11 20:35:12 +0000

    adding hashmap for headers to values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::env;
@@ -11,10 +12,24 @@ fn main() {
         Ok(f) => {
             println!("Successfully opened the file!");
             let reader = BufReader::new(f);
+            let mut columns: Vec<String> = Vec::new();
+            let mut columns_to_values: HashMap<String, Vec<String>> = HashMap::new();
+            let mut first_line_read = false;
             for line in reader.lines(){
                 match line {
                     Ok(l) => {
-                        println!("{}", l);
+                        if !first_line_read{
+                            let cols: Vec<&str> = l.split(",").collect();
+                            for c in cols {
+                                columns.push(c.to_string());
+                                columns_to_values.entry(c.to_string()).or_insert([].to_vec());
+                            }
+                            first_line_read = true;
+                        }
+                        let values: Vec<&str> = l.split(",").collect();
+                        let zipped: Vec<(&str, &str)> = columns.iter().zip(values.iter()).collect();
+                        // TODO: add values to the associated vecs
+
                     },
                     Err(_) => {print!("Error reading a line.");}
                 }
